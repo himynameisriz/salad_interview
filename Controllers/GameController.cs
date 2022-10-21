cd35c5f005eeeaf66ef782e9fffca110ca8a0ce0
Riz <v.rizwanali@gmail.com>
2022-10-21 09:09:41 -0600

    Giving all return values instead of FirstOrDefault, and enabling sort

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -8,6 +8,11 @@ public class GameController : ControllerBase
 {
     private IConfiguration _config;
     private readonly ILogger<GameController> _logger;
+    private readonly List<string> AvalabileOrdering = new List<string>()
+    {
+        "name", "released", "added", "created", "updated", "rating", "metacritic",
+        "-name", "-released", "-added", "-created", "-updated", "-rating", "-metacritic",
+    };
 
     public GameController(IConfiguration config, ILogger<GameController> logger)
     {
@@ -21,25 +26,34 @@ public class GameController : ControllerBase
         if (string.IsNullOrWhiteSpace(q)) 
         {
             _logger.Log(LogLevel.Warning, "No query was supplied");
-            return BadRequest();
+            return BadRequest("Please provide a query, such as '?q=undertale' in the query");
         }
-
-        var game = await GetGames(q, sort);
-        return game == null ? BadRequest("No games found") : Ok(game);
+        if (!string.IsNullOrWhiteSpace(sort) && !AvalabileOrdering.Contains(sort)) 
+        {
+            _logger.Log(LogLevel.Warning, $"Improper use of sort, with value '{sort}'");
+            return BadRequest("Improper use of sort, please use the following valid values: " + 
+                string.Join(",", AvalabileOrdering.Select(s => $"'{s}'")));
+        }
+        var games = await GetGames(q, sort);
+        return games == null ? BadRequest("No games found") : Ok(games);
     }
 
-    private async Task<Game> GetGames(string query, string sort)
+    private async Task<IEnumerable<Game>> GetGames(string query, string sort)
     {
 
         var (endpoint, key) = GetRawgApi();
         var paramString = $"?key={key}&search={query}";
+        if (!string.IsNullOrWhiteSpace(sort))
+        {
+            paramString += $"&ordering={sort}";
+        }
 
         _logger.Log(LogLevel.Debug, $"endpoint: {endpoint}, key: {key}, paramString: {paramString}");
 
         using var httpClient = new HttpClient();
         httpClient.BaseAddress = new Uri(endpoint);
         var rawgResponse = await httpClient.GetFromJsonAsync<RawgResponse>(paramString);
-        return rawgResponse.Results.FirstOrDefault();
+        return rawgResponse.Results;
     }
 
     private (string endpoint, string key) GetRawgApi() 
